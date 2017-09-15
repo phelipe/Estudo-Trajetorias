e784733cafaa11ef04188fc31b9d37cdb77ba622
Phelipe <phelipewesleydeoliveira@gmail.com>
2017-09-15 17:00:06 -0300

    adicionado código do sistema mecânico

--- src/main.jl
+++ src/main.jl
@@ -1,19 +1,14 @@
 push!(LOAD_PATH, pwd()*"/modules")
 
 using Trajetorias
-using PyPlot
+using Plots
+pyplot()
 
-tempo_final = 0.5
+tempo_final = 12
 coeficientes = minimumjerk(0,0,0,0,10,0,0,tempo_final)
 posicao, velocidade, aceleracao = functionform(coeficientes)
-
-x = 0:0.01:tempo_final
-y1 = map(posicao,x)
-y2 = map(velocidade,x)
-y3 = map(aceleracao,x)
-close("all")
-#plot(x,y1)
-#plot(x,y2)
-plot(x,y3)
-xlabel("tempo")
-ylabel("posição")
+time = 0:0.01:tempo_final
+p1 = plot(time, map(posicao,time), xlabel = "tempo", ylabel ="posição")
+p2 = plot(time, map(velocidade,time), xlabel = "tempo", ylabel ="velocidade")
+p3 = plot(time, map(aceleracao,time), xlabel = "tempo", ylabel ="aceleração")
+plot(p1, p2, p3, layout=(3,1))
