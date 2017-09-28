722a7d7ee7a5f978750f416e21c3e30e8d1cb9b4
Phelipe <phelipewesleydeoliveira@gmail.com>
2017-09-28 11:24:14 -0300

    adicionado parte do código para mecanismo de quatro barras

--- src/teste2.jl
+++ src/teste2.jl
@@ -1,7 +1,5 @@
-
 using RigidBodyDynamics
 using StaticArrays
-
 using CoordinateTransformations
 using GeometryTypes
 using DrakeVisualizer
@@ -12,5 +10,5 @@ mechanism = parse_urdf(Float64, urdf)
 state = MechanismState{Float64}( mechanism)
 DrakeVisualizer.any_open_windows() || DrakeVisualizer.new_window();
 vis = Visualizer(mechanism);
-set_configuration!(state,[10, 20, 20,20,20,20,0.])
+set_configuration!(state,[10, 20, 120,20,120,120,120.])
 settransform!(vis, state)
