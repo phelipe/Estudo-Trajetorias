062b40e2b73bb4605d7e3ebf488dbb9524a7f0fc
phelipe <phelipewesleydeoliveira@gmail.com>
2018-02-19 17:07:10 -0300

    corrigido o código do sinle, agora devo só conferir essa planta.

--- src/optimum-pid/simulation_singe.jl
+++ src/optimum-pid/simulation_singe.jl
@@ -1,13 +1,13 @@
 # aqui vou fazer o mesmo procedimento para simulação porém montando a equação para um pendulo simples
 #
 #
-
-
+using DifferentialEquations
+using Plots
+pyplot()
 type InputRobot{T} <: DEDataVector{T}
     x::Array{T,1}
     tau::Array{T,1}
 end
-
 
 #=
 Função que realiza a simulação
@@ -19,12 +19,15 @@ saídas :(as que irei precisar)
 e - somatório do erro de posição
 J - somatório do jerk
 =#
-function simulation(kp,kv,r)
-    if length(kp) != size
+function simulation(kp,kv,r,tend)
+    t0 = 0.
+    x_0 = [0.,0.]
+    v_0 = [0.,0.]
+    if length(kp) != 2
         error("O vetor de ganhos Kp deve ter mesma dimensão que a quantidade de juntas do robô ($(size))")
-    elseif length(kv) != size
+    elseif length(kv) != 2
         error("O vetor de ganhos Kv deve ter mesma dimensão que a quantidade de juntas do robô ($(size))")
-    elseif length(r) != size
+    elseif length(r) != 2
         error("O vetor de referência r deve ter mesma dimensão que a quantidade de juntas do robô ($(size))")
     end
     KP = diagm(kp)
@@ -32,8 +35,7 @@ function simulation(kp,kv,r)
     Ts = 0.02
 
     #função contendo o sistema, robô sendo utilizado
-    function robot(t,u,du)
-        #TODO: adicionar valores para esses termos
+    function robot(du,u,p,t)
         m1 = 0.2866
         L0 = 0.201
         L1 = 0.30997
@@ -55,30 +57,41 @@ function simulation(kp,kv,r)
         -g*l1*m1*sin(θ[2])]
 
         du[1:2] = dθ
-        du[3:4] = inv(M)*(tau - C*dθ -G)
+        du[3:4] = inv(M)*(u.tau - C*dθ -G)
     end
 
     function controlador(integrator)
-        # TODO: preciso ver como pegar os valores de erro de dentro da equação
-        e = q - r
         for c in user_cache(integrator)
-            c.tau = kp*e - kv*dotq
+            q = c.x[1:2]
+            dotq = c.x[3:4]
+            e = r - q
+            #println(integrator.t)
+            #println(dotq)
+            c.tau = KP*e - KV*dotq
+            println(c.tau)
         end
     end
 
     cbs = PeriodicCallback(controlador,Ts)
-    tspan = (t0,2)
+    tspan = (t0,tend)
     start = InputRobot(vcat(x_0,v_0), zeros(7))
     prob = ODEProblem(robot,start,tspan)
-    sol = solve(prob,Tsit5(),callback = cbs)
-
-    out_x = map(x -> x[1:size],sol.u) #posição
-    out_dx = map(x -> x[(size+1):end],sol.u)
+    sol = solve(prob,Tsit5(),callback = cbs,saveat = Ts/2)
+    out_x = map(x -> x[1:2],sol.u) #posição
+    out_dx = map(x -> x[3:end],sol.u)
     #velocidade
     #Aqui estou fazendo uma aproximação da aceleração e do jerk
     out_d2x = diff(out_dx)/Ts #aceleração
     out_d3x = diff(out_d2x)/Ts #jerk
 
-    #NOTE: agoa preciso escolher os valores a serem retornados para utiizar no otimizador
-
+    out_x, out_dx, out_d2x, out_d3x, sol.t
 end
+
+θ, ω, α, 𝚥, t = simulation([0., 0.],[0.,0.],[1.,1.],2);
+
+θ1 = map(x -> x[1],θ)
+θ2 = map(x -> x[2],θ)
+ω1 = map(x -> x[1],ω)
+ω2 = map(x -> x[2],ω)
+
+plot(t,teta1)
